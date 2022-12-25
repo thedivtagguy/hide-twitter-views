5f2c897f792e432cb887fdc620ba69c776c2b754
thedivtagguy <amanbhargava2001@gmail.com>
2022-12-25 19:43:38 +0530

    update

--- hide-views.js
+++ hide-views.js
@@ -1,10 +1,7 @@
 function hideViews() {
     console.log('Hiding views');
-    var views = document.querySelectorAll('[aria-label*="View Tweet analytics"]');
-    for (var i = 0; i < views.length; i++) {
-        var parentDiv = views[i].parentNode;
-        parentDiv.style.display = 'none';
-    }
+    [...document.querySelectorAll('[aria-label*="View Tweet analytics"]')].forEach(el => el.parentNode.style.display = 'none');
+
 
     // For single tweet pages (e.g. https://twitter.com/username/status/1234567890)
     // if (window.location.href.indexOf("status") > -1) {
